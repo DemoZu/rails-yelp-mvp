8dc22061313cb2c9400b33a05253314268467c27
Zuzanna Szymczak <zuzannaolgaszymczak@gmail.com>
2023-01-24 21:35:27 +0000

    RAILS 3 - chall 2 - half

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+  get 'restaurants/index', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/create', to: 'restaurants#create'
+  get 'restaurants/show', to: 'restaurants#show'
+  resources :restaurants
 end
