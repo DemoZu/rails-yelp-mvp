da7f4fb4ad10fca7069d8d7294db2d169ff2507d
Zuzanna Szymczak <zuzannaolgaszymczak@gmail.com>
2023-01-25 21:05:16 +0000

    Rails 3 - chal 2 up to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
   post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
-  get 'restaurants/index', to: 'restaurants#index'
+  get 'restaurants', to: 'restaurants#index'
   get 'restaurants/new', to: 'restaurants#new'
-  get 'restaurants/create', to: 'restaurants#create'
-  get 'restaurants/show', to: 'restaurants#show'
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id/', to: 'restaurants#show'
   resources :restaurants
 end
